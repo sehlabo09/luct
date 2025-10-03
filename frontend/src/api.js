2560265ce044ec68cbdaeb2ec7de27190d53440d
sehlabo09 <sehlabokhothalang0@gmail.com>
2025-10-03 21:26:30 +0200

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://luct-unfz.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
